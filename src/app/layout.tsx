1689321913606ed0d0f2f753d81514b0e1a542c9
Diego Mossmann <diegomossmann@MacBook-Pro-de-Diego.local>
2024-03-20 21:41:04 -0300

    revert pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '383937681161879');
+            fbq('init', '923279442518560');
             fbq('track', 'PageView');
             `,
           }}
